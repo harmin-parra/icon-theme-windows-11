48131359b8bc190cc9e4619c8edcb434d3834e2b
harmin-parra <harmin.parra@gmail.com>
2024-07-12 19:22:50 +0200

    utilitarian scripts

---
+++ nominal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# terminal
+mv 16/apps-utilities/_utilities-terminal.svg 16/apps-utilities/utilities-terminal.svg
+mv symbolic/apps/utilities-terminal.svg symbolic/apps/_utilities-terminal.svg
+
+# help
+mv 16/apps-settings/_help-about.svg 16/apps-settings/help-about.svg
+mv 16/actions/help-about.svg 16/actions/_help-about.svg
+mv 16/apps-settings/_help-browser.svg 16/apps-settings/help-browser.svg
+mv symbolic/apps/help-browser.svg symbolic/apps/_help-browser.svg
+
+# preferences-system
+mv 16/apps-settings/_preferences-system.svg 16/apps-settings/preferences-system.svg
+mv symbolic/apps/preferences-system.svg symbolic/apps/_preferences-system.svg
+
+# wallpaper
+mv 16/apps-settings/_preferences-desktop-wallpaper.svg 16/apps-settings/preferences-desktop-wallpaper.svg
+mv symbolic/apps/preferences-desktop-wallpaper.svg symbolic/apps/_preferences-desktop-wallpaper.svg
+
---
+++ xfce.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# terminal
+mv 16/apps-utilities/utilities-terminal.svg 16/apps-utilities/_utilities-terminal.svg
+mv symbolic/apps/_utilities-terminal.svg symbolic/apps/utilities-terminal.svg
+
+# help
+mv 16/apps-settings/help-about.svg 16/apps-settings/_help-about.svg
+mv 16/actions/_help-about.svg 16/actions/help-about.svg
+mv 16/apps-settings/help-browser.svg 16/apps-settings/_help-browser.svg
+mv symbolic/apps/_help-browser.svg symbolic/apps/help-browser.svg
+
+# preferences-system
+mv 16/apps-settings/preferences-system.svg 16/apps-settings/_preferences-system.svg
+mv symbolic/apps/_preferences-system.svg symbolic/apps/preferences-system.svg
+
+# wallpaper
+mv 16/apps-settings/preferences-desktop-wallpaper.svg 16/apps-settings/_preferences-desktop-wallpaper.svg
+mv symbolic/apps/_preferences-desktop-wallpaper.svg symbolic/apps/preferences-desktop-wallpaper.svg
+
